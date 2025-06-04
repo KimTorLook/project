ccd8285dac2072c97f71247f0da723bdf62e253a
KimTorLook <113922076+KimTorLook@users.noreply.github.com>
2025-06-05 03:25:12 +0800

    modify oder_app models

--- Kanteen/order_app/models.py
+++ Kanteen/order_app/models.py
@@ -1,3 +1,85 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from datetime import datetime
 
-# Create your models here.
+class School(models.Model):
+    class SchoolName(models.TextChoices):
+        SPCC = "SPCC", _("St. Paul's Co-educational College")
+        DGS = "DGS", _("Diocesan Girls' School")
+        HY = "HY", _("Heep Yunn School")
+        SMCC = "SMCC", _("St. Mary's Canossian College")
+        MCS = "MCS", _("Maryknoll Convent School(Secondary Section)")
+        SPCS = "SPCS", _("St. Paul's Convent School")
+        LSC = "LSC", _("La Salle College")
+        QC = "QC", _("Queen's College")
+        HKTATH = "HKTATH", _("Hong Kong Taoist Association Tang Hin Memorial Secondary School")
+        SKHTST = "SKHTST", _(" S.K.H. Tsang Shiu Tim Secondary School")
+
+    school_id = models.AutoField(primary_key=True)
+    school_name = models.CharField(
+        max_length=10,
+        choices=SchoolName.choices,
+        default=SchoolName.SPCC)
+    school_address = models.TextField(max_length=300)
+
+    def __str__(self):
+        return self.school_name
+    
+
+class Student(models.Model):
+    stid = models.AutoField(primary_key=True)
+    student_id = models.CharField(max_length=20, editable=False)
+    first_name = models.CharField(max_length=60, blank=False)
+    last_name = models.CharField(max_length=30, blank=False)
+    date_of_birth = models.DateTimeField(auto_now=False)
+    Email = models.EmailField(blank=False, unique=True)
+    school_id = models.ForeignKey(School, on_delete=models.CASCADE)
+    is_active=models.BooleanField(blank=False, null=False)
+    
+    def __str__(self):
+        return self.stid
+
+class Restaurant(models.Model):
+    restaurant_id = models.AutoField(primary_key=True)
+    restaurant_name = models.TextField()
+
+    def __str__(self):
+        return self.restaurant_id
+
+class Main_Course(models.Model):
+    main_course_id = models.AutoField(primary_key=True)
+    main_course_name = models.CharField(max_length=150)
+    main_course_cost = models.IntegerField()
+    main_course_price = models.IntegerField()
+
+    def __str__(self):
+        return self.main_course_id
+
+class Order(models.Model):
+    order_id = models.UUIDField(primary_key=True)
+    order_date_time = models.DateTimeField(auto_now=False)
+    payment_method = models.CharField(max_length=50)
+    confirm_payment = models.BooleanField()
+    meals = models.ManyToManyField(Main_Course)
+    school_id = models.ForeignKey(School, on_delete=models.CASCADE)
+
+    def get_choose(self):
+        return json.loads(self.choose)
+    
+    def set_choose(self):
+        self.choose = json.dumps(meal_)
+
+    def __str__(self):
+        return self.order_id
+
+    def load_menu(self):
+        pass
+        #menu_list = list_of_menu
+
+"""class Set_Menu(models.Model):
+    menu_id = models.AutoField(primary_key=True, default=0)
+    main_course_id = models.ForeignKey(Main_Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.restaurant_id
+"""
--- Kanteen/order_app/views.py
+++ Kanteen/order_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from order_app.models import Order, Main_Course
 
-# Create your views here.
+
+"""
+#ordering 的view code
+meal1 = Main_Course.objects.create(name="乾炒牛")
+meal2 = Main_Course.objects.create(name="abcxxx")
+
+# 創建產品並關聯標籤
+order = Order.objects.create(ord_id = "{}", papayment_methodyment = "COD")
+order = Order.meals.add(meal1, meal2)
+
+#查詢
+print(order.meals.all())  # 輸出: <QuerySet [<Tag: new>, <Tag: popular>]>
+
+
+#過濾
+Order.objects.filter(tags__name="new")
+"""
