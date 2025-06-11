c3d2640f873284a15b6212d88313af9eaa8f5935
KimTorLook <113922076+KimTorLook@users.noreply.github.com>
2025-06-11 19:49:24 +0800

    20250611 afterclass
    made a business logic for random select restaurant and random select meals

--- Kanteen/order_app/views.py
+++ Kanteen/order_app/views.py
@@ -8,21 +8,27 @@ def random_restaurent_for_5_days():
     for x in range(5):
         x = choices(restaurents)
         random_restaurent_list.append(x)  
-    return random_restaurent_list
+    return random_restaurent_list # <QuerySet [<Restaurant: Tai Hing>, <Restaurant: Tsui Wah>, <Restaurant: Tsui Ho>, <Restaurant: Ngan Lung>]>
 
-def get_main_course():
-    restaurant_list = random_restaurent_for_5_days()
-    random_main_course_for_a_day = []
-    for x in restaurant_list:
-        random_main_course_for_day_x=[]
-        random_main_course_1 = Main_Course.objects.get(restaurant = x)
-        random_main_course_2 = Main_Course.objects.get(restaurant = x)
-        random_main_course_for_day_x.append(random_main_course_1,random_main_course_2)
-        random_main_course_for_a_day.extend(random_main_course_for_day_x)
-    print(random_main_course_for_a_day)
+"""def get_main_course():
+    #loop the day 
+    list_for_the_week={}
+    restaurents = list(Restaurant.objects.all())
+    for day in range(1,6):
+        #random the restaurent
+        restaurent = choices(restaurents)
+        list_for_today = []
+        #random the meal1
+        main_course_1_list = list(Main_Course.objects.filter(Restaurant__restaurant_name = restaurent))
+        main_course_1 = choice(main_course_1_list)
+        #random the meal2
+        main_course_2_list = main_course_1_list.remove(main_course_1)
+        main_course_2 = choice(main_course_2_list)
+        list_for_today.append(main_course_1)
+        list_for_today.append(main_course_2)
+    list_for_the_week[day] = list_for_today
     # main_course = Main_Course.objects.filter(restaurant__restaurant_name="cafe de coral")
-
-
+"""
 
 def ordering(request):
          
@@ -31,27 +37,9 @@ def ordering(request):
         context = {
             "orders": orders,
             "random_restaurent_list":random_restaurent_for_5_days(),
-            "random_main_course" : get_main_course()
+            #"random_main_course" : get_main_course()
         }
 
         return render(request, "order_app/ordering.html", context)
     elif request.method == "POST":
          pass
-
-
-"""
-#ordering 的view code
-meal1 = Main_Course.objects.create(name="乾炒牛")
-meal2 = Main_Course.objects.create(name="abcxxx")
-
-# 創建產品並關聯標籤
-order = Order.objects.create(ord_id = "{}", papayment_methodyment = "COD")
-order = Order.meals.add(meal1, meal2)
-
-#查詢
-print(order.meals.all())  # 輸出: <QuerySet [<Tag: new>, <Tag: popular>]>
-
-
-#過濾
-Order.objects.filter(tags__name="new")
-"""
