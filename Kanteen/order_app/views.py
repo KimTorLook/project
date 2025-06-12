eb66278ae29054f786cea418a92b122416e340e8
KimTorLook <113922076+KimTorLook@users.noreply.github.com>
2025-06-12 21:32:29 +0800

    20250612 afterclass
    order_app的business logic 做好了抽選每日款式

--- Kanteen/order_app/views.py
+++ Kanteen/order_app/views.py
@@ -2,47 +2,63 @@ from django.shortcuts import render
 from order_app.models import Order, Main_Course, Restaurant
 from random import choice, choices
 
-def random_restaurent_for_5_days():
-    random_restaurent_list = []
-    restaurents = Restaurant.objects.all()
-    for x in range(5):
-        x = choices(restaurents)
-        random_restaurent_list.append(x)  
-    return random_restaurent_list # <QuerySet [<Restaurant: Tai Hing>, <Restaurant: Tsui Wah>, <Restaurant: Tsui Ho>, <Restaurant: Ngan Lung>]>
 
-"""def get_main_course():
+def get_main_course(): #random select 2 main courser for every weekday
     #loop the day 
-    list_for_the_week={}
+    list_for_the_week=[]
     restaurents = list(Restaurant.objects.all())
     for day in range(1,6):
         #random the restaurent
-        restaurent = choices(restaurents)
+        restaurent = choices(restaurents)  #done
+        print(restaurent)
         list_for_today = []
         #random the meal1
-        main_course_1_list = list(Main_Course.objects.filter(Restaurant__restaurant_name = restaurent))
-        main_course_1 = choice(main_course_1_list)
+        main_course_1_list = list(Main_Course.objects.filter(restaurant__restaurant_name = restaurent[0]))
+        main_course_1_list_str = []
+        for main_course in main_course_1_list:
+            main_course_str = str(main_course)
+            main_course_1_list_str.append(main_course_str)
+        main_course_1 = choice(main_course_1_list_str)
+        #print("main_course1",main_course_1)
+
         #random the meal2
-        main_course_2_list = main_course_1_list.remove(main_course_1)
-        main_course_2 = choice(main_course_2_list)
+        #print("course_1_list", main_course_1_list_str)
+        main_course_1_list_str.remove(main_course_1)
+        #print("course_2_list", main_course_1_list_str)
+        main_course_2 = choice(main_course_1_list_str)
+        #print("main_course2", main_course_1_list_str)
         list_for_today.append(main_course_1)
         list_for_today.append(main_course_2)
-    list_for_the_week[day] = list_for_today
+        list_for_the_week.append(list_for_today)
     # main_course = Main_Course.objects.filter(restaurant__restaurant_name="cafe de coral")
-"""
+    print(list_for_the_week)
+    return list_for_the_week
+
+def get_photo():
+    photo = Main_Course.objects.filter("xxxxx")
 
 def ordering(request):
-         
-    if request.method == "GET":
-        orders = Order.objects.all() 
-        context = {
-            "orders": orders,
-            "random_restaurent_list":random_restaurent_for_5_days(),
-            #"random_main_course" : get_main_course()
+    mainCourse = get_main_course()
+    context = {
+        "Monday_A" : mainCourse[0][0],
+        "Monday_B" : mainCourse[0][1],
+        #畀相
+        #畀錢
+        "Tuesday_A" : mainCourse[1][0],
+        "Tuesday_B" : mainCourse[1][1],
+        "Wensday_A" : mainCourse[2][0],
+        "Wensday_B" : mainCourse[2][1],
+        "Thuesday_A" : mainCourse[3][0],
+        "Thuesday_B" : mainCourse[3][1],
+        "Friday_A" : mainCourse[4][0],
+        "Friday_B" : mainCourse[4][1],
+        
         }
+    
+    return render(request, 'order_app/ordering.html', context)
 
-        return render(request, "order_app/ordering.html", context)
-    elif request.method == "POST":
-         pass
+
+
     
 def orderConfirmation(request):
     confirmedData = {
@@ -66,3 +82,16 @@ def orderConfirmation(request):
 
     return render(request, "order_app/orderConfirmation.html", context)
 
+#def ordering(request):
+         
+    if request.method == "GET":
+        orders = Order.objects.all() 
+        context = {
+            "orders": orders,
+            "random_restaurent_list":random_restaurent_for_5_days(),
+            #"random_main_course" : get_main_course()
+        }
+
+        return render(request, "order_app/ordering.html", context)
+    elif request.method == "POST":
+         pass
